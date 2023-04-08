fb6aba06d229c959703c1ee26f9372a8d391d668
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2023-04-08 15:26:57 +0800

    fixed types

--- src/pages/index2.tsx
+++ src/pages/index2.tsx
@@ -70,7 +70,7 @@ const AuthShowcase: React.FC = () => {
     <div className="flex flex-col items-center justify-center gap-4">
       <p className="text-center text-2xl text-white">
         {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
+        {secretMessage && <span> - {secretMessage.secretMessage}</span>}
       </p>
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
